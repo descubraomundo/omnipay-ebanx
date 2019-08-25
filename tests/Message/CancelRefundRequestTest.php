002f2d8e4e1aae2b09bf47a2458dc5600fde78c8
Gabriel Passarelli <passarelli.gabriel@gmail.com>
2019-08-25 17:41:30 -0300

    Added CancelRefund test

---
+++ tests/Message/CancelRefundRequestTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Omnipay\Ebanx\Message;
+
+use League\Ebanx\Test\TestCase;
+
+class CancelRefundRequestTest extends TestCase
+{
+
+    public function setUp()
+    {
+        $this->request = new CancelRefundRequest($this->getHttpClient(), $this->getHttpRequest());
+
+        $this->request->initialize(
+            array('transactionId' => 'ecc9be4512a')
+        );
+    }
+
+    public function testEndpoint()
+    {
+        $this->assertSame('https://api.ebanx.com.br/ws/refund?operation=cancel&merchant_refund_code=ecc9be4512a', $this->request->getEndpoint());
+    }
+
+
+    public function testGetDataIsEmpty()
+    {
+        $data    = $this->request->getData();
+
+        // Check required params
+        $this->assertEmpty($data);
+    }
+}
+
